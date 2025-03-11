a6bb17107d34e64449852b2b4241a1a83b48388d
Arshad vkb <arshadvkb118@gmail.com>
2025-03-11 04:24:49 -0700

    Refactor complaint submission toast messages for improved readability

--- lib/templates/user/complaints.dart
+++ lib/templates/user/complaints.dart
@@ -54,23 +54,13 @@ class _complaintsState extends State<complaints> {
                         if (response.statusCode == 200) {
                           print('Complaint submitted');
                           Fluttertoast.showToast(
-                              msg: "Complaint submitted",
-                              toastLength: Toast.LENGTH_SHORT,
-                              gravity: ToastGravity.BOTTOM,
-                              timeInSecForIosWeb: 1,
-                              backgroundColor: Colors.green,
-                              textColor: Colors.white,
-                              fontSize: 16.0);
+                            msg: "Complaint submitted",
+                          );
                         } else {
                           print('Failed to submit complaint');
                           Fluttertoast.showToast(
-                              msg: "Failed to submit complaint",
-                              toastLength: Toast.LENGTH_SHORT,
-                              gravity: ToastGravity.BOTTOM,
-                              timeInSecForIosWeb: 1,
-                              backgroundColor: Colors.red,
-                              textColor: Colors.white,
-                              fontSize: 16.0);
+                            msg: "Failed to submit complaint",
+                          );
                         }
                       }
                     }
