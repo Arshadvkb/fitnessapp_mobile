c95ff4f2e332a65f2d168b0c015638172b1a4a61
Arshad vkb <arshadvkb118@gmail.com>
2025-03-13 22:38:27 -0700

    Add payfees screen and implement navigation from ScheduleCard

---
+++ lib/templates/user/payfees.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class payfees extends StatelessWidget {
+  const payfees({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/templates/user/view_fee_details.dart
+++ lib/templates/user/view_fee_details.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:fitnessappnew/templates/user/payfees.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -125,7 +126,8 @@ class ScheduleCard extends StatelessWidget {
           SizedBox(height: 10),
           ElevatedButton(
             onPressed: () {
-              // Add your button action here
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => payfees()));
             },
             child: Text('Pay Now'),
           ),
