ccae51423382b3bc3dd24ffe3e352d20c8ec41bf
Arshad vkb <arshadvkb118@gmail.com>
2025-03-25 05:24:53 -0700

    Refactor import statement for user_menu in view_schedules.dart

--- lib/templates/user/view_schedules.dart
+++ lib/templates/user/view_schedules.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
-import 'package:fitnessappnew/templates/user/user_menu.dart';
+// import 'package:fitnessappnew/templates/user/user_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'user_menu.dart';
 
 class TimeSchedulePage extends StatefulWidget {
   const TimeSchedulePage({Key? key}) : super(key: key);
